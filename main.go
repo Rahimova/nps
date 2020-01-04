2bd96320631c4d6da8a739a25ea868d3b88a3efc
Admin <zebogulrahimova@gmail.com>
2020-01-04 12:38:09 +0500

    fixed: fixed#1

--- main.go
+++ main.go
@@ -3,9 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	//
 
-	 fmt.Println("Hello")
+	scores := [4]int {2, 4, 1, 14}
+
+
+	//Shift + f6  - переименование имени
+	var promotersLowerBound string = "hello"
+	// ctrl + alt + shift + левый клик мыши
+
+	 fmt.Println(scores[0])
+	 fmt.Println(scores[1])
+	 fmt.Println(promotersLowerBound)
 
 	}
 
